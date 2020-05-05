4aef6d66675c31419be1c8d2bdfc97084ebf123e
altmurzilka <altmurzilka@gmail.com>
2020-05-05 09:02:44 +0600

    Initial commit

---
+++ Simple_Table_View/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Simple_Table_View
+//
+//  Created by алтынпончик on 5/3/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var tutors: [Tutor] = []
+    
+    var body: some View {
+        NavigationView {
+            List(tutors) { tutor in
+                TutorCell(tutor: tutor)
+            }.navigationBarTitle(Text("Tutors"))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(tutors: testData)
+    }
+}
+
+struct TutorCell: View {
+    let tutor: Tutor
+    var body: some View {
+        return NavigationLink(destination: TutorDetail(name: tutor.name, headline: tutor.headline, bio: tutor.bio)) {
+            Image(tutor.imageName)
+                .cornerRadius(40)
+            VStack(alignment: .leading) {
+                Text(tutor.name)
+                Text(tutor.headline)
+                    .font(.subheadline).foregroundColor(.gray)
+            }
+        }
+    }
+}
---
+++ Simple_Table_View/TutorDetail.swift
@@ -0,0 +1,43 @@
+//
+//  TutorDetail.swift
+//  Simple_Table_View
+//
+//  Created by алтынпончик on 5/4/20.
+//  Copyright © 2020 алтынпончик. All rights reserved.
+//
+
+import SwiftUI
+
+struct TutorDetail: View {
+    
+    var name: String
+    var headline: String
+    var bio: String
+    
+    var body: some View {
+        VStack {
+            Image(name)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.orange, lineWidth: 4)
+            )
+                .shadow(radius: 10)
+            Text(name)
+                .font(.title)
+            Text(headline)
+                .font(.subheadline)
+            
+            Divider()
+            
+            Text(bio)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+        }.padding().navigationBarTitle(Text(name), displayMode: .inline)
+    }
+}
+
+struct TutorDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorDetail(name: "Simon Ng", headline: "Founder of AppCoda", bio: "Founder of AppCoda. Author of multiple iOS programming books including Beginning iOS 12 Programming with Swift and Intermediate iOS 12 Programming with Swift. iOS Developer and Blogger.")
+    }
+}
